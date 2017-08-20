7e820ce23abf6a6b93b7f207008979b1462836fe
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-08-20 15:24:00 +0900

    リッチテキストエディタを導入

---
+++ app/javascript/packs/admin/question-form.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import RichTextEditor from 'react-rte';
+
+export default class QuestionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: RichTextEditor.createEmptyValue()
+    }
+  }
+
+  onChange(value) {
+    this.setState({value});
+    if( this.props.onChange) {
+      this.props.onChange(
+        value.toString('html')
+      )
+    }
+  }
+
+  render() {
+    return (
+      <RichTextEditor
+        value ={this.state.value}
+        onChange={this.onChange.bind(this)}
+      />
+    )
+  }
+}
