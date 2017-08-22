059ea6246ec4087915fbca60f20cfde6f71201a6
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-08-23 00:24:10 +0900

    クイズ画面のUIを実装。

--- app/javascript/packs/question/answer-area.jsx
+++ app/javascript/packs/question/answer-area.jsx
@@ -1,47 +1,65 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React        from 'react'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
-import TextField from 'material-ui/TextField'
+import TextField    from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
+import QuizAction   from './quiz-action';
+import AnswerSnack  from './answer-snack';
+import ResultDialog from './result-dialog';
 
 export default class AnswerArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      questionNo: 1,
-      questionText: `abcede<strong>ZZZ</strong>aaa`
-    };
+  }
+
+  onChange(event) {
+    QuizAction.editingAnswer(event.target.value);
+  }
+
+  onClick() {
+    QuizAction.isCorrect(this.props.currentQuiz);
   }
 
   render() {
-    const cardStyle = { flex: '2', margin: '2%', boxShadow: 'rgba(0, 0, 0, 0.12) 2px 2px 10px 10px' };
-    const buttonStyle = { margin: '12px' };
-    const headerStyle = { borderBottom : '1px solid #e0e0e0' };
-    const titleStyle = { fontSize: '20px' };
     return (
-      <Card style={cardStyle}>
+      <Card className='answer-area__card'>
         <CardHeader
-          title={"Q" + this.state.questionNo + "."}
-          subtitle="Subtitle"
+          title={"Q" + this.props.currentNo + "."}
+          className='answer-area__header'
           actAsExpander={true}
           showExpandableButton={false}
-          style={headerStyle}
-          titleStyle={titleStyle}
+          titleStyle={{fontSize: '20px'}}
         />
-        <CardText expandable={false}>
-          {this.state.questionText}
-
+        <CardText expandable={false} dangerouslySetInnerHTML={{__html:this.props.currentQuiz.question}}>
         </CardText>
         <CardActions>
           <div>
             <TextField
               floatingLabelText="Input your answer"
               floatingLabelFixed={false}
+              onChange={this.onChange.bind(this)}
+              value={this.props.currentQuiz.answer}
             />
           </div>
-          <RaisedButton label="Answer" primary={true} style={buttonStyle} />
+          <RaisedButton
+            label="Answer"
+            primary={true}
+            className='answer-area__button'
+            onClick={this.onClick.bind(this)}
+          />
+        <AnswerSnack {...this.props.snackState} />
+        <ResultDialog {...this.props} />
         </CardActions>
       </Card>
     );
   }
 }
+
+AnswerArea.defaultProps = {
+  currentNo: 1,
+  currentQuiz: {
+    question_id: 0,
+    question:    '',
+    category:    1,
+    result:      'Unanswered',
+  }
+};
