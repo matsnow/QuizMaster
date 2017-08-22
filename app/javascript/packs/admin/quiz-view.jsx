9f14423c8ddb7f19e25309be98a7c9bc356446a2
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-08-23 00:22:23 +0900

    管理画面のクイズ1個の表示を整頓した。

--- app/javascript/packs/admin/quiz-view.jsx
+++ app/javascript/packs/admin/quiz-view.jsx
@@ -22,7 +22,7 @@ export default class QuizView extends React.Component {
   render() {
     return (
       <div className='quiz-view'>
-        <div className='quiz-view__question' dangerouslySetInnerHTML={{__html: 'Q. ' + this.props.question}}></div>
+        <div className='quiz-view__question' dangerouslySetInnerHTML={{__html: 'Q. ' + this.props.question.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'')}}></div>
         <div className='quiz-view__answer'>A. {this.props.answer}</div>
         <FlatButton label="Edit" onClick={this.editQuiz} className='quiz-view__button' />
         <FlatButton label="Delete" onClick={this.deleteQuiz} secondary={true} buttonStyle={{ borderRadius: '5px' }} className='quiz-view__button'/>
