dfc3c12700f03bf007a2ee8399f86e695dc87c89
Masayuki Matsuo <masayuki.matsuo@benesse-i-career.co.jp>
2017-08-23 18:00:10 +0900

    システム起動設定を修正した。

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker-compose run --rm web rails db:create
-docker-compose run --rm web rails db:migrate
+bundle exec rails log:clear
+bundle exec rails tmp:clear
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails s -p 3000 -b '0.0.0.0' -e development
